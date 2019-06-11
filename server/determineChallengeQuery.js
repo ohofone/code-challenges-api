21a7b257db8498c2d95ebce82dfdf68a532abff0
Jag <jagdeepsingh3939@gmail.com>
2019-06-11 14:36:16 -0700

    datatype and difficulty can be any case

--- server/determineChallengeQuery.js
+++ server/determineChallengeQuery.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Determines challenge sql query given data type and difficulty
  * @module
@@ -13,25 +15,28 @@ module.exports = (params) => {
   let query = '';
   let values = [];
 
-  if ((params.dataType === 'anyDatatype' || params.dataType === undefined) &&
-      (params.difficulty === 'anyDifficulty' || params.difficulty === undefined)) {
+  let dataType = params.dataType ? params.dataType.toLowerCase() : undefined;
+  let difficulty = params.difficulty ? params.difficulty.toLowerCase() : undefined;
+
+  if ((dataType === 'anydatatype' || dataType === undefined) &&
+      (difficulty === 'anydifficulty' || difficulty === undefined)) {
     // any datatype challenge of any difficulty
     query = 'SELECT * FROM challenges ORDER BY random() LIMIT 1;';
 
-  } else if (params.dataType === 'anyDatatype') {
+  } else if (dataType === 'anydatatype') {
     // Any datatype challenge with certain difficulty
     query = `SELECT * FROM challenges WHERE difficulty=$1 ORDER BY random() LIMIT 1;`;
-    values = [params.difficulty];
+    values = [difficulty];
 
-  } else if (params.difficulty === 'anyDifficulty' || params.difficulty === undefined) {
+  } else if (difficulty === 'anydifficulty' || difficulty === undefined) {
     // Any difficulty challenge with certain data type
     query = `SELECT * FROM challenges WHERE dataType=$1 ORDER BY random() LIMIT 1;`;
-    values = [params.dataType];
+    values = [dataType];
 
   } else {
     // challenge of certain data type and difficulty
     query = `SELECT * FROM challenges WHERE dataType=$1 AND difficulty=$2 ORDER BY random() LIMIT 1;`;
-    values = [params.dataType, params.difficulty];
+    values = [dataType, difficulty];
   }
   return { query, values };
 };
