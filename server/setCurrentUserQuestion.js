99ba540ac9f2763318c845998d60476a57daf273
Jag <jagdeepsingh3939@gmail.com>
2019-06-11 15:13:16 -0700

    add user test

--- server/setCurrentUserQuestion.js
+++ server/setCurrentUserQuestion.js
@@ -9,7 +9,11 @@ module.exports = (user_id, question_id) => {
   queryDatabase(userCheckQuery, userValues)
     .then((user) => {
       if (!user.length) {
-        addNewUser(user_id, question_id).then(() => {
+        console.log('--------------------------------------------------------------------------------');
+        console.log(addNewUser(user_id));
+        console.log('--------------------------------------------------------------------------------');
+
+        addNewUser(user_id).then(() => {
           updateCurrentQuestion(user_id, question_id);
         });
       } else {
