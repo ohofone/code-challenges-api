9992056e1011738786fbc316b28af170cb68d849
Bonnie Wang <jcwang118@hotmail.com>
2019-06-13 11:23:27 -0700

    testing travis

--- __tests__/updateTime.test.js
+++ __tests__/updateTime.test.js
@@ -9,16 +9,20 @@ describe('update time', () => {
   it('should update the time if resonable time has passed', (done) => {
     return supertest(app)
       .get(`/question`)
-      .then(testQuestion => {
+      .then((testQuestion) => {
         //random number from 11 to 15 representing how mand minutes were spent solving the problem
-        const NEW_TIME = Math.ceil(Math.random() * 5) + 10; 
-        return updateTime(testQuestion.body, NEW_TIME)
-          .then(updatedQuestion => {
-            expect(testQuestion.length !== 0 && testQuestion[0].avg_time !== updatedQuestion.avg_time).toBeTruthy();
+        const NEW_TIME = Math.ceil(Math.random() * 5) + 10;
+        return updateTime(testQuestion.body, NEW_TIME).then(
+          (updatedQuestion) => {
+            expect(
+              testQuestion.length !== 0 &&
+                testQuestion[0].avg_time !== updatedQuestion.avg_time
+            ).toBeTruthy();
             done();
-          });
+          }
+        );
       })
-      .catch(error => {
+      .catch((error) => {
         done();
       });
   });
