a0474c902e6c21778233a0f935d391aaa29184e1
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-13 15:18:46 -0700

    tests updated to use heroku

--- __tests__/updateTime.test.js
+++ __tests__/updateTime.test.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 describe('update time', () => {
   it('should update the time if resonable time has passed', (done) => {
     return supertest(app)
-      .get(`/question`)
+      .get(`${herokuUrl}/question`)
       .then((testQuestion) => {
         //random number from 11 to 15 representing how mand minutes were spent solving the problem
         const NEW_TIME = Math.ceil(Math.random() * 5) + 10;
