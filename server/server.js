5387f70b1ca254f19cf0ae081900da0753d7d929
Bonnie Wang <jcwang118@hotmail.com>
2019-06-14 15:16:05 -0700

    adding swagger docs

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+const swagger = require('swagger-ui-express');
+const swaggerDocs = require('../docs/config/swagger.json');
+
 const determineChallengeQuery = require('./determineChallengeQuery.js');
 const queryDatabase = require('./queryDataBase.js');
 const getCurrentQuestion = require('./getCurrentQuestion.js');
@@ -24,22 +27,23 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/question/:dataType?/:difficulty?/:userID?', getChallenge);
 app.get('/currentQuestion/:userID', getCurrentQuestion);
 app.get('/solution/:userID', getSolution);
 
 app.use('/docs', express.static('docs'));
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocs));
 
 /**
  * Gets the correct code challenge based on user input
- * 
- * @param {object} - request 
- * @param {object} - response 
+ *
+ * @param {object} - request
+ * @param {object} - response
  */
 function getChallenge(request, response) {
-  let {query, values} = determineChallengeQuery(request.params);
+  let { query, values } = determineChallengeQuery(request.params);
 
   queryDatabase(query, values)
     .then((question) => {
@@ -48,7 +52,7 @@ function getChallenge(request, response) {
       }
       return question;
     })
-    .then(question => {
+    .then((question) => {
       response.send(question);
     });
 }
